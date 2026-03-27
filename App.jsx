17108da5fead915f4a087459eeab410e0ed774af
Rahul <161291475+rahul-1342@users.noreply.github.com>
2026-03-27 10:43:44 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import ChatBox from "./components/ChatBox";
+import InputBox from "./components/InputBox";
+
+function App() {
+  return (
+    <div className="app">
+      <h2>AI Chatbox</h2>
+      <ChatBox />
+      <InputBox />
+    </div>
+  );
+}
+
+export default App;
